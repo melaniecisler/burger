77f0fc6b6accc222fe86a6773ca7777478d45438
Melanie Cisler <melanie.cisler@yahoo.com>
2020-10-12 12:02:30 -0500

    initial commit

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,11 @@
 const mysql = require("mysql");
 
-let connection;
-
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
+let connection = mysql.createConnection({
         host: "localhost",
+        port: 3306,
         database: "burger_db",
         user: "root",
-        port: 3306,
         password: "TitoBurrito1!"
     });
-}
 
 module.exports = connection;
