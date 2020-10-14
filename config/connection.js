967cdf6997d4ef28ea23bf3867d0b039fb75bd5f
Melanie Cisler <melanie.cisler@yahoo.com>
2020-10-14 14:22:03 -0500

    added jaws

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 const mysql = require("mysql");
 
-let connection =  mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  let connection =  mysql.createConnection({
         host: "localhost",
         port: 3306,
         database: "burger_db",
         user: "root",
         password: "TitoBurrito1!"
     });
+}
+
 
 module.exports = connection;
